cd4782a49b70dea92a56c2c6ab55e155d37b2ac1
Risa Takata <beat1422@icloud.com>
2020-01-31 23:37:59 -0800

    add models for signin

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+const uuidv1 = require('uudd/v1')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim:
+    }
+})
