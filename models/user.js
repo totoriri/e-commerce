01c49121fbdfa3d158ba1fad3a92dfbdd588b51e
Risa Takata <beat1422@icloud.com>
2020-02-03 21:35:36 -0800

    created user schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,35 @@ const uuidv1 = require('uudd/v1')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        trim:String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: 32
+    },
+    hashed_password: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: 32
+    },
+    about: {
+        type: String,
+        trim: true,
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
     }
-})
+},
+    {timestamps:true}
+)
